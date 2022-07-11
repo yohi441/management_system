19d9f65293f3ee8af56179120f944be06ffded90
yohi <rikou92991@gmail.com>
2022-07-11 14:43:02 +0800

    modfy settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -7,7 +7,7 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-SECRET_KEY = 'django-insecure--ss*8(rgf9khxz!+$o@ub4u@%(xen2q)hak=1!=npy=)=6-1gq'
+SECRET_KEY = config('SECRET_KEY')
 
 
 DEBUG = config('DEBUG', default=False, cast=bool)
