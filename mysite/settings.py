ba5914c8c10f59c640a1a1d85e04999ae1e4b829
yohi <cirilo@ingenuity.ph>
2022-10-10 09:56:42 +0800

    Add forfiet list in sidebar

--- mysite/settings.py
+++ mysite/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+ALLOWED_HOSTS = '*'
 USE_L10N = True
 
 USE_THOUSAND_SEPARATOR = True
